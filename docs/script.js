0b66f7c1e7e62856c94639708b19383496b20eac
Yaroslav04 <miroshnichenko.jarik@gmail.com>
2025-01-08 01:16:37 +0200

    Adapted for mobile format

--- docs/script.js
+++ docs/script.js
@@ -1,15 +1,15 @@
 function validateForm() {
-  var name = document.getElementById("name").value;
-  var email = document.getElementById("email").value;
-  var message = document.getElementById("message").value;
+  var name = document.getElementById("name").value.trim();
+  var email = document.getElementById("email").value.trim();
+  var message = document.getElementById("message").value.trim();
   var errorMessage = document.getElementById("error-message");
   var successMessage = document.getElementById("success-message");
 
-  if (name === "" || email === "" || message === "") {
+  if (!name || !email || !message) {
     errorMessage.textContent = "Будь ласка, заповніть всі поля.";
     errorMessage.style.display = "block";
     successMessage.style.display = "none";
-    setTimeout(function () {
+    setTimeout(() => {
       errorMessage.style.display = "none";
     }, 5000);
     return false;
@@ -21,23 +21,24 @@ function validateForm() {
       "Невірна адреса електронної пошти. Перевірте формат.";
     errorMessage.style.display = "block";
     successMessage.style.display = "none";
-    setTimeout(function () {
+    setTimeout(() => {
       errorMessage.style.display = "none";
     }, 5000);
     return false;
   }
 
-  successMessage.textContent = "Форма надіслана успішно!";
+  successMessage.textContent = "Дякуємо! Ми отримали вашу заявку.";
   successMessage.style.display = "block";
   errorMessage.style.display = "none";
 
+  // Очищення полів форми
   document.getElementById("name").value = "";
   document.getElementById("email").value = "";
   document.getElementById("message").value = "";
 
-  setTimeout(function () {
+  setTimeout(() => {
     successMessage.style.display = "none";
   }, 5000);
 
-  return false;
+  return false; // Залишаємо false, щоб уникнути перезавантаження сторінки
 }
