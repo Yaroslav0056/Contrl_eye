052b23fd769009f423eebfea5eec3bd10268743b
Yaroslav04 <miroshnichenko.jarik@gmail.com>
2025-01-17 18:39:24 +0200

    Improvements, corrections and adaptation for mobile format

--- docs/script.js
+++ docs/script.js
@@ -1,43 +1,42 @@
 function validateForm() {
-  var name = document.getElementById("name").value.trim();
-  var email = document.getElementById("email").value.trim();
-  var message = document.getElementById("message").value.trim();
-  var errorMessage = document.getElementById("error-message");
-  var successMessage = document.getElementById("success-message");
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const message = document.getElementById("message").value.trim();
+  const errorMessage = document.getElementById("error-message");
+  const successMessage = document.getElementById("success-message");
 
   if (!name || !email || !message) {
-    errorMessage.textContent = "Будь ласка, заповніть всі поля.";
-    errorMessage.style.display = "block";
-    successMessage.style.display = "none";
-    setTimeout(() => {
-      errorMessage.style.display = "none";
-    }, 5000);
+    displayMessage(errorMessage, "Будь ласка, заповніть всі поля.");
     return false;
   }
 
-  var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   if (!emailPattern.test(email)) {
-    errorMessage.textContent =
-      "Невірна адреса електронної пошти. Перевірте формат.";
-    errorMessage.style.display = "block";
-    successMessage.style.display = "none";
-    setTimeout(() => {
-      errorMessage.style.display = "none";
-    }, 5000);
+    displayMessage(errorMessage, "Невірна адреса електронної пошти. Перевірте формат.");
     return false;
   }
 
-  successMessage.textContent = "Дякуємо! Ми отримали вашу заявку.";
-  successMessage.style.display = "block";
-  errorMessage.style.display = "none";
+  displayMessage(successMessage, "Дякуємо! Ми отримали вашу заявку.", true);
 
   document.getElementById("name").value = "";
   document.getElementById("email").value = "";
   document.getElementById("message").value = "";
 
-  setTimeout(() => {
-    successMessage.style.display = "none";
-  }, 5000);
-
   return false;
 }
+
+function displayMessage(element, message, isSuccess = false) {
+  element.textContent = message;
+  element.style.display = "block";
+
+  const otherElement = isSuccess
+    ? document.getElementById("error-message")
+    : document.getElementById("success-message");
+
+  otherElement.style.display = "none";
+
+  setTimeout(() => {
+    element.style.display = "none";
+  }, 5000);
+}
+
